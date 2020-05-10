6707e78db1041cc6fdb88dd3c3a31c41486271f7
Bolis10xD <ing.orodriguez98@gmail.com>
2020-05-09 21:20:17 -0500

    Firma de los métodos de la interfaz

--- src/main/java/com/uatx/inventarios/service/InventarioService.java
+++ src/main/java/com/uatx/inventarios/service/InventarioService.java
@@ -1,6 +1,17 @@
 package com.uatx.inventarios.service;
 
+import com.uatx.inventarios.dto.AltaInventarioDTO;
+import com.uatx.inventarios.dto.BajaInventarioDTO;
+import com.uatx.inventarios.dto.ProductoDTO;
+
+import java.util.List;
+
 public interface InventarioService {
+
+    Long storeAltaInventario(AltaInventarioDTO altaInventarioDTO);
+    Long storeBajaInventario(BajaInventarioDTO bajaInventarioDTO, Long productoID);
+    List<ProductoDTO> findAltasByProducto(Long productoID);
+    List<ProductoDTO> findBajasByProducto(Long productoID);
 
 
 }
