f19f74219a2f7b8509cf42e9875ef6c53f7cfae8
Bolis10xD <ing.orodriguez98@gmail.com>
2020-05-09 21:14:55 -0500

    Controladores agregados

--- src/main/java/com/uatx/inventarios/controller/ProductosController.java
+++ src/main/java/com/uatx/inventarios/controller/ProductosController.java
@@ -1,20 +1,34 @@
 package com.uatx.inventarios.controller;
 
+import com.uatx.inventarios.dto.AltaInventarioDTO;
+import com.uatx.inventarios.dto.ImagenDTO;
 import com.uatx.inventarios.dto.ProductoDTO;
+import com.uatx.inventarios.model.Producto;
+import com.uatx.inventarios.service.InventarioService;
 import com.uatx.inventarios.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductosController {
 
+    @Autowired
+    private InventarioService inventarioService;
 
     @Autowired
     private ProductoService productoService;
+
+    @PostMapping("/realizarAlta")
+    @ResponseBody
+    public Long realizarAlta(@RequestBody AltaInventarioDTO altaInventarioDTO) {
+        return inventarioService.storeAltaInventario(altaInventarioDTO);
+    }
 
     @PostMapping("/guardar")
     @ResponseBody
@@ -33,6 +47,33 @@ public class ProductosController {
     public List<ProductoDTO> findAllWithImg() {
         return productoService.findProdWithImage();
     }
+
+    @GetMapping("find/productodto/{id}")
+    @ResponseBody
+    public ProductoDTO windProductoWithIdFechtImg(@PathVariable(value = "id") Long id){
+        return productoService.findByIdWithImage(id);
+    }
+
+    @GetMapping("/alta/{id}")
+    public String findById(@PathVariable(value = "id") String id, Map<String, Object> model, RedirectAttributes flash){
+        ProductoDTO productoDTO = productoService.findByIdWithImage(Long.parseLong(id));
+        ImagenDTO imagenDTO = productoDTO.getImagen();
+
+
+
+
+        if (productoDTO == null) {
+            flash.addFlashAttribute("error", "El Producto no existe en la base de datos");
+            return "redirect:/consulta-productos";
+        }
+
+        model.put("producto", productoDTO);
+        model.put("base64",imagenDTO.getDataBase64());
+        model.put("type",imagenDTO.getMimeType());
+        model.put("titulo", "Detalle del Producto: " + productoDTO.getNombre());
+        return "alta-producto";
+    }
+
 
     @GetMapping("/find/by-name")
     @ResponseBody
