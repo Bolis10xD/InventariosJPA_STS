e9015232e0f6707c4889482de8163141f90da6d7
Bolis10xD <ing.orodriguez98@gmail.com>
2020-05-17 10:16:55 -0500

    Metodo de buscar alta por id con el preoducto creado

--- src/main/java/com/uatx/inventarios/repository/AltaInventarioRepository.java
+++ src/main/java/com/uatx/inventarios/repository/AltaInventarioRepository.java
@@ -1,10 +1,16 @@
 package com.uatx.inventarios.repository;
 
 import com.uatx.inventarios.model.AltaInventario;
+import com.uatx.inventarios.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AltaInventarioRepository extends JpaRepository<AltaInventario,Long> {
 
+    @Query("SELECT a FROM AltaInventario a JOIN FETCH a.producto where a.producto.id = :ID")
+    List<AltaInventario> findAltaIdFetchProducto(Long ID);
 }
